b1ee67f9697f3d5317d9f28c41592813f2b589dd
Joyasree Paul <joyasree27@gmail.com>
2026-08-22 12:04:21 +0600

    Add token definitions with final update

--- Token.h
+++ Token.h
@@ -16,3 +16,55 @@ enum TokenType
     T_IF,
     T_ELSE,
     T_ELSE_IF,
+    T_WHILE,
+
+    // Identifiers and values
+    IDENTIFIER,
+    NUMBER,
+    FLOAT_LITERAL,
+    CHAR_LITERAL,
+    STRING_LITERAL,
+
+    // Arithmetic operators
+    PLUS,
+    MINUS,
+    MUL,
+    DIV,
+
+    // Assignment
+    ASSIGN,
+
+    // Relational operators
+    LT,
+    GT,
+    LE,
+    GE,
+    EQ,
+    NE,
+
+    // Brackets
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+
+    // Other
+    SEMICOLON,
+
+    END_OF_FILE,
+    UNKNOWN
+};
+
+struct Token
+{
+    TokenType type;
+    string lexeme;
+
+    Token(TokenType t, const string &l)
+    {
+        type = t;
+        lexeme = l;
+    }
+};
+
+#endif
