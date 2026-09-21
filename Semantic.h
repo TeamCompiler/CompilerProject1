5bc2f35bfeb38ae180b47dbfd0609c096b7782d6
Eshana Suzat <eshana.suzat4045@gmail.com>
2026-09-21 17:42:22 +0600

    semantic definitions done

--- Semantic.h
+++ Semantic.h
@@ -4,30 +4,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#include "AST.h"
+
 class SemanticAnalyzer
 {
 private:
 
-    // Variable name -> type
     unordered_map<string, string> symbolTable;
 
-    // Variable name -> known value
     unordered_map<string, string> valueTable;
 
-public:
 
+    // Declare a variable
     void declare(
         const string &id,
         const string &type
     );
 
+
+    string analyzeExpression(
+        ExprPtr expr
+    );
+
+
+    void analyzeStatement(
+        StmtPtr stmt
+    );
+
+
+    void analyzeStatements(
+        const vector<StmtPtr> &statements
+    );
+
+
+public:
+
+    void analyze(
+        ProgramPtr program
+    );
+
+
     bool exists(
         const string &id
     );
 
+
     string getType(
         const string &id
     );
+
 
     bool checkAssignment(
         const string &variableType,
@@ -35,19 +60,23 @@ public:
         const string &variableName
     );
 
+
     string checkArithmetic(
         const string &leftType,
         const string &rightType
     );
+
 
     void setValue(
         const string &id,
         const string &value
     );
 
+
     bool isZero(
         const string &id
     );
+
 
     bool isZeroValue(
         const string &value
